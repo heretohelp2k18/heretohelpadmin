6e8b028f7d0a7eacd2a586b9efb064517d05f73e
jethro <jethro@kayweb.com.au>
2018-11-08 23:39:21 +0800

    updating admin with user id = 1 to be superadmin

--- sql/logs/11-08-18.sql
+++ sql/logs/11-08-18.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `app_users` ADD `is_superadmin` TINYINT(1) NOT NULL DEFAULT '0' AFTER `is_admin`, ADD `skipchatbot` TINYINT(1) NOT NULL DEFAULT '0' AFTER `is_superadmin`;
+
+UPDATE `app_users` SET `is_superadmin` = '1' WHERE `app_users`.`id` = 1;
