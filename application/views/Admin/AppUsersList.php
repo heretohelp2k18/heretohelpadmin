4de79c3c9d23546f80a4b5ac4b88f1f47e7971ea
jethro <jethro@kayweb.com.au>
2018-11-08 23:46:40 +0800

    User admin can not be deleted

--- application/views/Admin/AppUsersList.php
+++ application/views/Admin/AppUsersList.php
@@ -6,9 +6,13 @@ echo "
     <td>$username</td>
     <td>$position</td>
     <td>
-        <a class='action-link edit_app_user' data-id='$id'><i class='fa fa-pencil-square-o' aria-hidden='true'></i></a>
-        <a class='action-link delete_app_user' data-id='$id'><i class='fa fa-trash-o' aria-hidden='true'></i></a>
-    </td>
+        <a class='action-link edit_app_user' data-id='$id'><i class='fa fa-pencil-square-o' aria-hidden='true'></i></a>";
+    if($is_superadmin == 0)
+    {
+        echo "<a class='action-link delete_app_user' data-id='$id'><i class='fa fa-trash-o' aria-hidden='true'></i></a>";
+    }
+                
+echo "</td>
 </tr>
 ";
 ?>
